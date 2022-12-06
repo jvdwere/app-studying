eb9daf888109f1eb85c796642a2587547ea60ee8
jvdwere <joao.victordwere@gmail.com>
2022-12-06 15:10:57 -0300

    estilização da pagina e Drawer

--- src/modules/Answers/index.tsx
+++ src/modules/Answers/index.tsx
@@ -16,10 +16,11 @@ import {
 	AlertDescription,
 	Container,
 	Heading,
-	Card, 
-	CardHeader, 
-	CardBody, 
-	CardFooter
+	Card,
+	CardHeader,
+	CardBody,
+	CardFooter,
+	Image,
 } from '@chakra-ui/react';
 import { statesList } from '../../helpers/states';
 
@@ -126,52 +127,70 @@ function Respostas() {
 				<Box w="100%">
 					<Box>
 						<FormLabel>
-							
-							<Box p="16px" fontSize="18pt">
+
+							<Box p="16px" fontSize="18pt" textAlign='center' >
 								Naturalidades do Brasil.
 							</Box>
-							<FormLabel fontSize="16pt" >
-								Em que estado vc nasceu?
-							</FormLabel>
-							<Flex
-								flexDirection={{ base: 'column', md: 'row' }}
-								justifyContent={{ base: 'none', md: 'space-between' }}
-								w={{ base: '100%', md: '600px' }}
-								border="1px solid red"
-							>
-								<Flex alignItems="center">
-									<Select
-										w="250px"
-										bgColor="white"
-										name="select"
-										placeholder="Selecione um Estado"
-										onChange={(event) => setEstado(event.target.value)}
-									>
-										{statesList.map((s) => (
-											<>
-												<option key={s.state} value={s.stateValue}>
-													{s.state}
-												</option>
-											</>
-										))}
-									</Select>
-								</Flex>
 
-								<Flex mt={{ base: '8px', md: '0px' }} alignItems='center'>
-									<Button mr="8px" colorScheme="teal" onClick={handleClick}>
-										Verificar
-									</Button>
-									<Button
-										colorScheme="blue"
-										onClick={() => setEstadoSelecionado('')}
+							<Card w='650px' align='center' bg='#00AEAE' marginLeft="315px">
+								<CardHeader>
+									<Box bg="white" w='280px' textAlign='center' borderRadius='10px'>
+										<Heading size='md'>Em que estado vc nasceu?</Heading>
+									</Box>
+								</CardHeader>
+								<CardBody>
+									<Flex
+										flexDirection={{ base: 'column', md: 'row' }}
+										justifyContent={{ base: 'none', md: 'space-between' }}
+										w={{ base: '100%', md: '600px' }}
 									>
-										Limpar Verificação
-									</Button>
-								</Flex>
-							</Flex>
-							<FormLabel>
-								{estadoSelecionado && <Heading m='8px'>{estadoSelecionado}</Heading>}
-							</FormLabel>
+
+										<Flex alignItems="center">
+											<Select
+												w="250px"
+												bgColor="white"
+												name="select"
+												placeholder="Selecione um Estado"
+												onChange={(event) => setEstado(event.target.value)}
+											>
+												{statesList.map((s) => (
+													<>
+														<option key={s.state} value={s.stateValue}>
+															{s.state}
+														</option>
+													</>
+												))}
+											</Select>
+										</Flex>
+
+										<Flex mt={{ base: '8px', md: '0px' }} alignItems='center'>
+											<Button mr="8px" colorScheme="teal" onClick={handleClick}>
+												Verificar
+											</Button>
+											<Button
+												colorScheme="blue"
+												onClick={() => setEstadoSelecionado('')}
+											>
+												Limpar Verificação
+											</Button>
+										</Flex>
+
+									</Flex>
+								</CardBody>
+							</Card>
+							<Card w='650px' align='center' bg='white' marginLeft="315px">
+								<CardBody>
+									<FormLabel>
+										{estadoSelecionado && <Heading m='8px' bg='white' borderRadius='10px'>{estadoSelecionado}</Heading>}
+									</FormLabel>
+									<Box boxSize='sm'>
+										<Image borderRadius='full'
+											w='250px'
+											src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDQwvNCqW3Vqw5l9Ha_Phutm1QomrEPVseow&usqp=CAU'
+											alt='Brasil' />
+									</Box>
+								</CardBody>
+							</Card>
 						</FormLabel>
 					</Box>
 				</Box>
